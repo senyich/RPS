cfbbd2b7c0fd5671d9957d265d5548e33e3f3c6a
toptalk.messenger@gmail.com <arseniyneshetin@gmail.com>
2025-01-22 13:15:38 +0700

    create game class

--- Program.cs
+++ Program.cs
@@ -8,3 +8,40 @@ internal class Program
         
     }
 }
+
+public class Game
+{
+    private bool isGameEnded=false;
+    private Random randomMove;
+    private int userMove;
+    private int computerMove;
+    private string winner;
+    private string[] moves = { "Камень", "Ножницы", "Бумага" };
+    private int p1 = 0;
+    private int p2 = 0;
+    private void StartGame()
+    {
+        while (isGameEnded != true)
+        {
+            userMove = UserMove();
+        }
+    }
+    private int UserMove()
+    {
+        Console.WriteLine("Введите свой ход:\n" +
+                "1 - камень\n" +
+                "2 - ножницы\n" +
+                "3 - бумага");
+        int move = int.Parse(Console.ReadLine());
+        if (!new int[] { 1, 2, 3 }.Contains(move))
+        {
+            Console.WriteLine("Введите корректный ход!");
+            UserMove();
+        }
+        else
+            return move;
+        return 0;
+    }
+    private void OnGameEnded(string winner) => Console.WriteLine("Игра окончена");
+    private void EndGame() => isGameEnded = true;
+}
