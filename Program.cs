8aa82263ef0ab097718e83b8c0cd11d0f48ff747
toptalk.messenger@gmail.com <arseniyneshetin@gmail.com>
2025-01-22 13:28:40 +0700

    save completed program

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿
 
 
+using System.Runtime.InteropServices;
+
 internal class Program
 {
     static void Main(string[] args)
     {
-        
+        Game game = new Game();
+        game.StartGame();
     }
 }
 
@@ -19,12 +22,53 @@ public class Game
     private string[] moves = { "Камень", "Ножницы", "Бумага" };
     private int p1 = 0;
     private int p2 = 0;
-    private void StartGame()
+    public Game()
+    {
+        randomMove = new Random();
+    }
+    public void StartGame()
     {
         while (isGameEnded != true)
         {
             userMove = UserMove();
+            computerMove = randomMove.Next(1, 4);
+            int winner = CheckWinner(userMove, computerMove);
+            switch(winner)
+            {
+                case 1:
+                    Console.WriteLine($"Победил Человек! Он выйграл с {moves[userMove - 1]} над {moves[computerMove-1]}");
+                    isGameEnded = true;
+                    break;
+                case 2:
+                    Console.WriteLine($"Победил Компьютер! Он выйграл с {moves[computerMove - 1]} над {moves[userMove - 1]}");
+                    isGameEnded = true;
+                    break;
+                case 0:
+                    Console.WriteLine($"Победила ничья, у всех был ход: {moves[computerMove - 1]}");
+                    isGameEnded = true;
+                    break;
+                case -1:
+                    Console.WriteLine($"Никто не победил, все обиделись");
+                    isGameEnded = true;
+                    break;
+            }
         }
+        OnGameEnded();
+    }
+    private int CheckWinner(int p1, int p2)
+    {
+        if (p1 == 1 && p2 == 1) return 0;
+        else if (p1 == 1 && p2 == 2) return 1;
+        else if (p1 == 1 && p2 == 3) return 2;
+
+        else if (p1 == 2 && p2 == 1) return 2;
+        else if (p1 == 2 && p2 == 2) return 0;
+        else if (p1 == 2 && p2 == 3) return 1;
+
+        else if (p1 == 3 && p2 == 1) return 1;
+        else if (p1 == 3 && p2 == 2) return 2;
+        else if (p1 == 3 && p2 == 3) return 0;
+        return -1;
     }
     private int UserMove()
     {
@@ -33,15 +77,16 @@ public class Game
                 "2 - ножницы\n" +
                 "3 - бумага");
         int move = int.Parse(Console.ReadLine());
-        if (!new int[] { 1, 2, 3 }.Contains(move))
+        if (!(new int[3] { 1, 2, 3 }).Contains(move))
         {
             Console.WriteLine("Введите корректный ход!");
+            Console.Clear();
             UserMove();
         }
         else
             return move;
         return 0;
     }
-    private void OnGameEnded(string winner) => Console.WriteLine("Игра окончена");
+    private void OnGameEnded() => Console.WriteLine("Игра окончена");
     private void EndGame() => isGameEnded = true;
 }
